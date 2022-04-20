ec2cbb1eabdbb48f9794a91f1ac68f734ec6cdba
Ansu26 <dimar_suarez82182@elpoli.edu.co>
2022-04-20 09:57:03 -0500

    Sexto Commit, sexta parte terminada. Se agregan funciones para detectar cuán la pelota es golpeada y una función que detecta las colisiones. Juego terminado funcionando correctamente.
    Changes to be committed:
    	modified:   main.js

--- main.js
+++ main.js
@@ -27,6 +27,9 @@
         this.speedX = 3;
         this.board = board;
         this.direction = 1;
+        this.bounceAngle = 0;
+        this.maxBounceAngle = Math.PI /12;
+        this.speed = 3;
 
         board.ball = this;
         this.kind = "circle"
@@ -36,6 +39,25 @@
         move : function(){
             this.x += (this.speedX * this.direction);
             this.y += (this.speedY);
+        },
+        get width(){
+            return this.radius * 2;
+        },
+        get height(){
+            return this.radius * 2;
+        },
+        collision : function(bar){
+            let relativeIntersectY = (bar.y + (bar.height / 2)) - this.y;
+            let normalizedIntersectY = relativeIntersectY / (bar.height / 2);
+            this.bounceAngle = normalizedIntersectY * this.maxBounceAngle;
+            this.speedY = this.speed * -Math.sin(this.bounceAngle);
+            this.speedX = this.speed * Math.cos(this.bounceAngle);
+
+            if(this.x > (this.board.width /2)){
+                this.direction = -1;
+            }else {
+                this.direction = 1;
+            }
         }
     }
 })();
@@ -85,13 +107,48 @@
                 draw(this.ctx,el);
             }
         },
+        check_collisions: function(){
+            for (let i = this.board.bars.length - 1; i >= 0; i--) {
+                let bar = this.board.bars[i];
+
+                if(hit(bar, this.board.ball)){
+                    this.board.ball.collision(bar);
+                }
+            }
+        },
         play: function(){
             if(board.playing){
                 this.clean();
                 this.draw();
+                this.check_collisions();
                 this.board.ball.move();
             }
         }
+    }
+
+    function hit(a, b){
+        let hit = false;
+        //Colisiones horizontales
+        if(b.x + b.width >= a.x && b.x < a.x + a.width){
+            //Colisiones verticales
+            if(b.y + b.height >= a.y && b.y < a.y + a.height){
+                hit = true;
+            }
+        }
+        //Colision de a con b
+        if(b.x <= a.x && b.x + b.width >= a.x + a .width){
+            if(b.y <= a.y && b.y + b.height  >= a.y + a.height){
+                hit = true;
+            }
+        }
+         //Colision de b con a
+         if(a.x <= b.x && a.x + a.width >= b.x + b .width){
+            if(a.y <= b.y && a.y + a.height  >= b.y + b.height){
+                hit = true;
+            }
+        }
+
+        return hit;
     }
 
     function draw(ctx, element){
