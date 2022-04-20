e34ef21ebee0547ecbd2f2e123715fdd57690ca3
Ansu26 <dimar_suarez82182@elpoli.edu.co>
2022-04-20 08:21:10 -0500

    Segundo commit, segunda parte terminada. Creación de las barras con forma de rectángulo.
    Changes to be committed:
    	modified:   main.js

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 (function(){
-    var self
-        class Board {
-        constructor(width, height) {
+    self.Board = function(width,height){
             this.width = width;
             this.height = height;
             this.playing = false;
@@ -9,16 +7,38 @@
             this.bars = [];
             this.ball = null;
         }
+        
+    self.Board.prototype = {
+        get elements(){
+            let elements = this.bars;
+            elements.push(this.ball);
+            return elements;
+        }
     }
 })();
 
-self.Board.prototype = {
-    get elements(){
-        var elements = this.bars;
-        elements.push(ball);
-        return elements;
+(function(){
+    self.Bar = function(x,y,width,height,board){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.board = board;
+
+        this.board.bars.push(this);
+
+        this.kind = "rectangle";
     }
-}
+
+    self.Bar.prototype = {
+        down : function(){
+
+        },
+        up : function(){
+
+        }
+    }
+})();
 
 (function(){
     self.BoardView = function(canvas,board){
@@ -28,12 +48,36 @@ self.Board.prototype = {
         this.board = board;
         this.ctx = canvas.getContext("2d");
     }
+
+    self.BoardView.prototype = {
+        draw: function(){
+            for (let i = this.board.elements.length - 1; i >= 0; i--) {
+                let el = this.board.elements[i];
+
+                draw(this.ctx,el);
+            }
+        }
+    }
+
+    function draw(ctx,element){
+        if(element !== null && element.hasOwnProperty("kind")){
+            switch(element.kind){
+                case "rectangle":
+                    ctx.fillRect(element.x, element.y, element.width, element.height);
+                    break;
+            }
+        }
+    }
 })();
 
 window.addEventListener("load", main);
 
 function main(){
-    var board = new Board(800,400);
-    var canvas = document.getElementById('canvas');
-    var boardView = new BoardView(canvas,board)
+    let board = new Board(800,400);
+    let bar = new Bar(20,100,40,100,board);
+    let bar2 = new Bar(750,100,40,100,board);
+    let canvas = document.getElementById('canvas');
+    let boardView = new BoardView(canvas,board);
+
+    boardView.draw();
 }
