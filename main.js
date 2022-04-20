12a91ba998478e2d6267be5910a3e1a2b43b6719
Ansu26 <dimar_suarez82182@elpoli.edu.co>
2022-04-20 09:04:04 -0500

    Cuarto commit, cuarta parter completada. Movimiento gráfico de las barras y creación de la pelota con sus respectivos métodos y de forma gráfica.
    Changes to be committed:
    	modified:   main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 (function(){
-    self.Board = function(width,height){
+    self.Board = function(width, height){
             this.width = width;
             this.height = height;
             this.playing = false;
@@ -18,7 +18,21 @@
 })();
 
 (function(){
-    self.Bar = function(x,y,width,height,board){
+    self.Ball = function(x, y, radius, board){
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.speedY = 0;
+        this.speedX = 3;
+        this.board = board;
+
+        board.ball = this;
+        this.kind = "circle"
+    }
+})();
+
+(function(){
+    self.Bar = function(x, y, width, height, board){
         this.x = x;
         this.y = y;
         this.width = width;
@@ -26,7 +40,7 @@
         this.board = board;
         this.board.bars.push(this);
         this.kind = "rectangle";
-        this.speed = 10;
+        this.speed = 5;
     }
 
     self.Bar.prototype = {
@@ -43,7 +57,7 @@
 })();
 
 (function(){
-    self.BoardView = function(canvas,board){
+    self.BoardView = function(canvas, board){
         this.canvas = canvas;
         this.canvas.width = board.width;
         this.canvas.height = board.height;
@@ -52,44 +66,61 @@
     }
 
     self.BoardView.prototype = {
+        clean: function(){
+            this.ctx.clearRect(0, 0, this.board.width, this.board.height);
+        },
         draw: function(){
             for (let i = this.board.elements.length - 1; i >= 0; i--) {
                 let el = this.board.elements[i];
 
                 draw(this.ctx,el);
             }
+        },
+        play: function(){
+            this.clean();
+            this.draw();
         }
     }
 
-    function draw(ctx,element){
-        if(element !== null && element.hasOwnProperty("kind")){
-            switch(element.kind){
-                case "rectangle":
-                    ctx.fillRect(element.x, element.y, element.width, element.height);
-                    break;
-            }
+    function draw(ctx, element){
+        switch(element.kind){
+            case "rectangle":
+                ctx.fillRect(element.x, element.y, element.width, element.height);
+                break;
+            case "circle":
+                ctx.beginPath();
+                ctx.arc(element.x, element.y, element.radius, 0, 7);
+                ctx.fill();
+                ctx.closePath();
+                break;
         }
+        
     }
 })();
 
-var board = new Board(800,400);
-var bar = new Bar(20,100,40,100,board);
-var bar2 = new Bar(735,100,40,100,board);
+var board = new Board(800, 400);
+var bar = new Bar(20, 100, 40, 100, board);
+var bar2 = new Bar(735, 100, 40, 100, board);
 var canvas = document.getElementById('canvas');
-var boardView = new BoardView(canvas,board);
+var boardView = new BoardView(canvas, board);
+var ball = new Ball(350, 100, 10, board);
 
 document.addEventListener("keydown",(ev) => {
+    ev.preventDefault();
     if(ev.key === "ArrowUp"){
-        bar.up();
+        bar2.up();
     }else if(ev.key === "ArrowDown"){
+        bar2.down();
+    }else if(ev.key === "w"){
+        bar.up();
+    }else if(ev.key === "s"){
         bar.down();
     }
-
-    console.log(bar.toString());
 })
 
-window.addEventListener("load", main);
+window.requestAnimationFrame(controller);
 
-function main(){
-    boardView.draw();
+function controller(){
+    boardView.play();
+    window.requestAnimationFrame(controller);
 }
